591f762c0d49c674e38bfdbd6905e3c1e55f5110
Ho3einWave <ho3ein8280@gmail.com>
2024-10-22 16:52:55 +0330

    stlye: modal style bug fixes

--- lib/components/SwapButton/WaitingForWallet.tsx
+++ lib/components/SwapButton/WaitingForWallet.tsx
@@ -4,6 +4,8 @@ import { useThemeStore } from "../../store/theme.store";
 import { IoClose } from "react-icons/io5";
 import { ModalState, useSwapStore } from "../../store/swap.store";
 import "./WaitingForWallet.scss";
+import { motion } from "framer-motion";
+
 const WaitingForWallet = () => {
     const wallet = useTonWallet();
     const { setModalState } = useSwapStore();
@@ -13,7 +15,10 @@ const WaitingForWallet = () => {
     };
     const { colors } = useThemeStore();
     return (
-        <div
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
             className="waiting-for-wallet-container"
             style={{ color: colors.text_black }}
         >
@@ -26,7 +31,7 @@ const WaitingForWallet = () => {
                 Confirm the transaction in {wallet?.device.appName}
             </div>
             <p>It will only take a moment</p>
-        </div>
+        </motion.div>
     );
 };
 

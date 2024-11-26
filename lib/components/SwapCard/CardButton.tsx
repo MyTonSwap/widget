610ef718ce5764b9586459859ca6c2902e262e2a
Ho3einWave <ho3ein8280@gmail.com>
2024-11-26 16:10:45 +0330

    style: update styles for transparency and clean up code formatting

--- lib/components/SwapCard/CardButton.tsx
+++ lib/components/SwapCard/CardButton.tsx
@@ -1,14 +1,14 @@
-import { FC, PropsWithChildren } from "react";
-import clsx from "clsx";
-import { MdKeyboardArrowDown } from "react-icons/md";
-import "./CardButton.scss";
-import { useOptionsStore } from "../../store/options.store";
-import { useTranslation } from "react-i18next";
-import { useSwapStore } from "../../store/swap.store";
+import { FC, PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import './CardButton.scss';
+import { useOptionsStore } from '../../store/options.store';
+import { useTranslation } from 'react-i18next';
+import { useSwapStore } from '../../store/swap.store';
 type CardButtonProps = {
     isLoading: boolean;
     onClick: () => void;
-    type: "pay" | "receive";
+    type: 'pay' | 'receive';
 };
 
 const CardButton: FC<CardButtonProps & PropsWithChildren> = ({
@@ -22,8 +22,8 @@ const CardButton: FC<CardButtonProps & PropsWithChildren> = ({
     const { isSelectingToken } = useSwapStore();
     const isDisabled = (() => {
         if (isSelectingToken) return true;
-        if (type === "pay" && options.lock_pay_token) return true;
-        if (type === "receive" && options.lock_receive_token) return true;
+        if (type === 'pay' && options.lock_pay_token) return true;
+        if (type === 'receive' && options.lock_receive_token) return true;
         return false;
     })();
     return (
@@ -31,42 +31,42 @@ const CardButton: FC<CardButtonProps & PropsWithChildren> = ({
             disabled={isDisabled}
             onClick={isDisabled ? () => {} : onClick}
             {...{
-                ...(isLoading && type === "pay"
-                    ? { "data-skeleton": true }
+                ...(isLoading && type === 'pay'
+                    ? { 'data-skeleton': true }
                     : {}),
             }}
             data-testid={`card-button-${type}`}
             className={clsx(
-                "selection-box-container",
-                isLoading && "loading",
-                type === "pay" && isLoading && "pay-loading",
-                type === "receive" && isLoading && "receive-loading"
+                'selection-box-container',
+                isLoading && 'loading',
+                type === 'pay' && isLoading && 'pay-loading',
+                type === 'receive' && isLoading && 'receive-loading'
             )}
             style={{
-                ...(isDisabled && { opacity: 0.7, cursor: "auto" }),
-                ...(isLoading && type === "pay"
+                ...(isDisabled && { opacity: 0.7, cursor: 'auto' }),
+                ...(isLoading && type === 'pay'
                     ? { color: `var(--text-black-color)` }
                     : {
                           background: `var(--input-token-color)`,
                           color: `var(--text-black-color)`,
                       }),
                 ...{
-                    "--skeleton-bg": `var(--input-token-color)`,
-                    "--skeleton-shine": `var(--skeleton-shine-color)`,
+                    '--skeleton-bg': `var(--input-token-color)`,
+                    '--skeleton-shine': `var(--skeleton-shine-color)`,
                 },
             }}
         >
             {!isLoading ? (
                 children
-            ) : type === "receive" ? (
+            ) : type === 'receive' ? (
                 <>
-                    {t("select")}{" "}
+                    {t('select')}{' '}
                     <div className="dropdown-icon">
                         <MdKeyboardArrowDown />
                     </div>
                 </>
             ) : (
-                ""
+                ''
             )}
         </button>
     );
